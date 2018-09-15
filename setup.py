3b7ba3646fc0d12dcce7ce5f8c4cd0f99995d4d0
Shoppon <shopppon@gmail.com>
2018-09-15 11:44:59 +0800

    添加打包配置

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='shoppon',
-      version='1.0',
+      version='0.1',
       description='Demo code',
-      url='http://github.com/shoppon/shoppon',
+      url='https://github.com/shoppon/shoppon/',
       author='Shoppon',
       author_email='shoppon@gmail.com',
       license='MIT',
